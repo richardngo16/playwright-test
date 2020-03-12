32f45fa4515e06f438c596906adbdaaaaded3525
richardngo16 <richardngo16@gmail.com>
2020-03-12 12:31:53 -0700

    fixed tests

--- e2e/pageObjects/index.js
+++ e2e/pageObjects/index.js
@@ -1,14 +1,15 @@
-import { browserType, launchConfig, contextConfig } from '../playwright.config'
+import { browserType, launchConfig, contextConfig } from "../playwright.config";
 
-const rootSelector = '#root';
+const rootSelector = "#root";
 let browser, context, page;
 
 export const root = async () => await page.$(rootSelector);
 
 export const load = async () => {
-    browser = await browserType.launch(launchConfig);
-    context = await browser.newContext(contextConfig);
-    page = await context.newPage(baseURL);
+  browser = await browserType.launch(launchConfig);
+  context = await browser.newContext(contextConfig);
+  page = await context.newPage();
+  await page.goto(baseURL);
 };
 
 export const close = async () => await browser.close();
